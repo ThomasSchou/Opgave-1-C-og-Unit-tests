eafb0a2dd9d0ebde6fa518a5da480dab46d76059
ThomasSchou <thom63t4@edu.easj.dk>
2021-10-05 13:31:05 +0200

    Add project files.

---
+++ Class Library og Unit Test/FootBallPlayer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Class_Library_og_Unit_Test
+{
+    public class FootBallPlayer
+    {
+        private int _id = 0;
+        private string _name;
+        private double _price;
+        private int _shirtNumber;
+
+        public FootBallPlayer(string name, double price, int shirtNumber)
+        {
+            ID = ID++;
+            Name = name;
+            Price = price;
+            ShirtNumber = shirtNumber;
+        }
+
+        public int ID
+        {
+            get => _id;
+            set
+            {
+                if (value < 1) { throw new ArgumentOutOfRangeException(); }
+
+                _id = value;
+            }
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (value.Length < 4) { throw new ArgumentOutOfRangeException(); }
+
+                _name = value;
+            }
+        }
+
+        public double Price
+        {
+            get => _price;
+            set
+            {
+                if (value < 0) { throw new ArgumentOutOfRangeException(); }
+                _price = value;
+            }
+        }
+
+        public int ShirtNumber
+        {
+            get => _shirtNumber;
+            set
+            {
+                if (value < 1 || value >= 100) { throw new ArgumentOutOfRangeException(); }
+
+                _shirtNumber = value;
+            }
+        }
+    }
+}
---
+++ Class Library og Unit TestTests/FootBallPlayerTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Class_Library_og_Unit_Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Library_og_Unit_Test.Tests
+{
+    [TestClass()]
+    public class FootBallPlayerTests
+    {
+
+        //[ClassInitialize()]
+        //public static void setup(TestContext context)
+        //{
+        //    FootBallPlayer footBallPlayer = new FootBallPlayer("Jack", 1500, 3);
+        //}
+
+        [TestMethod()]
+        public void TestID()
+        {
+            FootBallPlayer footBallPlayer = new FootBallPlayer("Jack", 1500, 3);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(
+                (() => footBallPlayer.Name = "Bo"));
+        }
+        [TestMethod()]
+        public void TestName()
+        {
+            Assert.Fail();
+        }
+        [TestMethod()]
+        public void TestPrice()
+        {
+            Assert.Fail();
+        }
+        [TestMethod()]
+        public void TestShirtNumber()
+        {
+            Assert.Fail();
+        }
+    }
+}
